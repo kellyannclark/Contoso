5165a21208ea83ba70bc2973400611d106323fe3
kellyannclark <mrs.kellyannclark@gmail.com>
2024-03-16 22:13:09 -0600

    Re-add project files excluding ignored files

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,38 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        // The Students and Instructors DbSet are not strictly necessary for TPH,
+        // but you can include them for easier querying.
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            // You do not need to map Student and Instructor to "Person" here as EF Core handles it.
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+            // People is the table that will be used for TPH inheritance.
+            modelBuilder.Entity<Person>().ToTable("Person");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
+    }
+}
